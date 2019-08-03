463e117bae4a7b31e798209f6131646cd8ca4954
Acer <pamoshakishani@gmail.com>
2019-08-03 15:21:18 +0530

    notices updated

---
+++ app/src/main/java/com/mad/lk/NoticeAddNotices.java
@@ -0,0 +1,14 @@
+package com.mad.lk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoticeAddNotices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice_add_notices);
+    }
+}
