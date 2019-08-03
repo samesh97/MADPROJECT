1781da096af6380e4cf4456082260c9705d59426
Acer <pamoshakishani@gmail.com>
2019-08-03 21:23:01 +0530

    Merge branch 'master' of C:\Users\Acer\AndroidStudioProjects\MADPROJECT with conflicts.

---
+++ app/src/main/java/com/mad/lk/NoticeAdminDisplayNotices.java
@@ -0,0 +1,32 @@
+package com.mad.lk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class NoticeAdminDisplayNotices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice_admin_display_notices);
+
+        Intent intentGet = getIntent();
+        String date = intentGet.getStringExtra("msg1");
+        String film = intentGet.getStringExtra("msg2");
+        String description=intentGet.getStringExtra("msg3");
+
+        TextView Date= findViewById(R.id.textViewDisplayDate);
+        Date.setText(date);
+
+        TextView Film=findViewById(R.id.textViewDisplayFilm);
+        Film.setText(film);
+
+        TextView Description=findViewById(R.id.textViewDisplayDescription);
+        Description.setText(description);
+
+
+    }
+}
