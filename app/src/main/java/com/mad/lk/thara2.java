01b79f0d79772b74ca26956b10f254d8541ee4dc
Lalindu97 <lalindub@gmail.com>
2019-08-05 11:53:56 +0530

    tharashaMsgNew

--- app/src/main/java/com/mad/lk/thara2.java
+++ app/src/main/java/com/mad/lk/thara2.java
@@ -2,7 +2,9 @@ package com.mad.lk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class thara2 extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class thara2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thara2);
+    }
+
+    public void thara1(View view) {
+        startActivity(new Intent(thara2.this,thara1.class));
     }
 }
