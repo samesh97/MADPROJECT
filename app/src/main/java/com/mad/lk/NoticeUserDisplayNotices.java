e53d7fe3fb247a6e7aba8ea69ea1e288985bbf15
Acer <pamoshakishani@gmail.com>
2019-08-05 11:32:51 +0530

    updated  notice user interface

---
+++ app/src/main/java/com/mad/lk/NoticeUserDisplayNotices.java
@@ -0,0 +1,14 @@
+package com.mad.lk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoticeUserDisplayNotices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice_user_display_notices);
+    }
+}
