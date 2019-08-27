c6e940ec5e64d637c32f0e8658e0f63cc1cd8f3d
TharashaMiyu <tharasha.miyu@gmail.com>
2019-08-27 23:20:33 +0530

    Agu27

--- app/src/main/java/com/mad/lk/dbFilmDetails.java
+++ app/src/main/java/com/mad/lk/dbFilmDetails.java
@@ -1,22 +1,25 @@
 package com.mad.lk;
 
-import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
-import android.provider.BaseColumns;
 
-import java.util.List;
+
+import android.widget.TableRow;
 
 
 public class dbFilmDetails extends SQLiteOpenHelper {
 
 
         private static String DATABASE_NAME = "Users.db";
+
+        //------------------------------------------------------------------
         private static String TABLE_NAME = "Film_Details";
+
+        //-------------------------------------------------------------------
         public static final String COL_1 = "FilmID";
         public static final String COL_2 = "FilmName";
         public static final String COL_3 = "Role1";
@@ -24,142 +27,106 @@ public class dbFilmDetails extends SQLiteOpenHelper {
         public static final String COL_5 = "Role3";
         public static final String COL_6 = "Role4";
         public static final String COL_7 = "DirectorName";
+        public static final String COL_8 = "Photo1";
+        public static final String COL_9 = "Photo2";
+        public static final String COL_10 = "Photo3";
+        public static final String COL_11 = "Photo4";
+
+        private  DatabaseHelper db;
 
 
-    public dbFilmDetails(Context context)
-    {
+
+    public dbFilmDetails(Context context) {
         super(context, DATABASE_NAME, null, 1);
+        SQLiteDatabase db = this.getWritableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        try {
+        db.execSQL("CREATE TABLE " + TABLE_NAME +
+                "FilmID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "FilmName VARCHAR(50),Role1 VARCHAR(50)," +
+                " Role2 VARCHAR(50)," +
+                " Role3 VARCHAR(50), " +
+                " Role4 VARCHAR(50), " +
+                " DirectorName VARCHAR(50)," +
+                " Photo1 Image," +
+                " Photo2 Image," +
+                " Photo3 Image," +
+                " Photo4 Image)");
 
-        db.execSQL("CREATE TABLE Film_Details (FilmID INTEGER PRIMARY KEY AUTOINCREMENT, FilmName VARCHAR(50),Role1 VARCHAR(50), Role2 VARCHAR(50),  " +
-                "Role3 VARCHAR(50),  Role4 VARCHAR(50),  DirectorName VARCHAR(50))");
-        }catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
     {
-        db.execSQL("DROP TABLE IF EXISTS FILMDETAILS");
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
     }
+//------------------------------------------------------------------------
 
-
-    public boolean Add_FilmDetails(String FilmID, String FilmName, String Role1, String Role2, String Role3, String Role4, String DirectorName)
-    {
-
-        SQLiteDatabase db = this.getWritableDatabase();
+    public void Add_FilmDetails(String filmName, String role1, String role2, String role3, String role4, String directorName) {
 
         ContentValues values = new ContentValues();
-        values.put(COL_1, FilmID);
-        values.put(COL_2, FilmName);
-        values.put(COL_3, Role1);
-        values.put(COL_4, Role2);
-        values.put(COL_5, Role3);
-        values.put(COL_6, Role4);
-        values.put(COL_7, DirectorName);
 
-        Long newRowId = db.insert(TABLE_NAME,null,values);
-        db.insert(TABLE_NAME, null, values);
-        db.close();
+        values.put(COL_2, filmName);
+        values.put(COL_3, role1);
+        values.put(COL_4, role2);
+        values.put(COL_5, role3);
+        values.put(COL_6, role4);
+        values.put(COL_7, directorName);
+        values.put(COL_8, photo1);
+        values.put(COL_9, Photo2);
+        values.put(COL_10, Photo3);
+        values.put(COL_10, Photo4);
 
-        if(newRowId == -1)
-        {
-            return false;
-        }
-        else
-        {
-            return true;
-        }
-    }
-
-
-
-
-    public boolean Update_FilmDetails(String id,String username,String password,String email)
-    {
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        ContentValues contentValues = new ContentValues();
-        contentValues.put("NAME", username);
-        contentValues.put("EMAIL", email);
-        contentValues.put("PASSWORD", password);
-
-
-        if(db.update(TABLE_NAME, contentValues, "ID=?", new String[] {id}) == 0)
-        {
-            return false;
-        }
-        else
-        {
-            return true;
-        }
-
+        db.insert(TABLE_NAME,null,contentValues);
 
     }
-    public boolean Delete_FilmDetails(String id)
-    {
-        SQLiteDatabase db = this.getWritableDatabase();
-        int val = db.delete(TABLE_NAME, "ID =?", new String[]{id});
 
-        if(val > 0)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
+    public Cursor getFilmInformation(SQLiteDatabase db){
 
-    public Cursor getAllData_FilmDetails()
-    {
-        SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME,null);
+        Cursor cursor;
+        String[] projections = {COL_2,COL_3,COL_4,COL_5,COL_6,COL_7,COL_8,COL_9,COL_10,COL_11};
+        cursor = db.query(TABLE_NAME,projections,null,null,null,null,null);
         return cursor;
     }
 
-    public boolean IsExist_FilmName_FilmDetails(String name)
-    {
-        SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE FilmName = '" + name + "'",null);
-        if(cursor.getCount() == 0)
-        {
-            cursor.close();
-            return false;
-        }
-        else
-        {
-            cursor.close();
-            return true;
-        }
+
+    public Cursor getVideo(String filmName,SQLiteDatabase sqLiteDatabase){
+        String[] projections = {COL_1, COL_2, COL_3, COL_4, COL_5, COL_6, COL_7, COL_8, COL_9, COL_10, COL_11};
+        String selection = COL_2+" LIKE ?";
+        String[] selection_args = {filmName};
+        Cursor cursor = sqLiteDatabase.query(TABLE_NAME,projections,selection,selection_args,null,null,null);
+        return cursor;
+    }
+
+    public int Update_FilmDetails(String oldfilmName,String newfilmName, String oldrole1, String newrole1,
+                                   String oldrole2, String newrole2, String oldrole3, String newrole3,
+                                   String oldrole4, String newrole4, String olddirectorName, String newdirectorName,SQLiteDatabase sqLiteDatabase) {
+        ContentValues contentValues = new ContentValues();
+
+        ContentValues.put(COL_2, newfilmName);
+        ContentValues.put(COL_3, newrole1);
+        ContentValues.put(COL_4, newrole2);
+        ContentValues.put(COL_5, newrole3);
+        ContentValues.put(COL_6, newrole4);
+        ContentValues.put(COL_7, newdirectorName);
+
+
+        String selection = COL_2+" LIKE ?";
+        String[] selection_args = {oldfilmName};
+        int count = sqLiteDatabase.update(TABLE_NAME,contentValues,selection,selection_args);
+
+        return count;
+    }
+    public void Delete_FilmDetails(String filmname,SQLiteDatabase sqLiteDatabase) {
+        String selection = COL_1 + " LIKE ?";
+        String[] selection_args = {filmname};
+        sqLiteDatabase.delete(TABLE_NAME, selection, selection_args);
 
     }
 
-    public List void readAllInfo()
-    {
-
-        SQLiteDatabase db = this.getWritableDatabase();
-
-         String[] projection = new String[]{
-                 dbFilmDetails.COL_1,
-                 dbFilmDetails.COL_2,
-                 dbFilmDetails.COL_3,
-                 dbFilmDetails.COL_4,
-                 dbFilmDetails.COL_5,
-                 dbFilmDetails.COL_6,
-                 dbFilmDetails.COL_7
-         };
-        String sortOrder = dbFilmDetails.COL_1+ "FilmID";
-
-return ;
-
-    }
 }
 
 
--- app/src/main/java/com/mad/lk/film_details_maintain.java
+++ app/src/main/java/com/mad/lk/film_details_maintain.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,12 +26,16 @@ public class film_details_maintain extends AppCompatActivity {
     EditText txtrole4;
     EditText txtdirector;
 
-    TextView viewFilmName;
-    TextView viewRole1;
-    TextView viewRole2;
-    TextView viewRole3;
-    TextView viewRole4;
-    TextView viewDirector;
+    TextView image1;
+    TextView image2;
+    TextView image3;
+    TextView image4;
+
+    ImageView viewimage1;
+    ImageView viewimage2;
+    ImageView viewimage3;
+    ImageView viewimage4;
+
 
 
 
@@ -46,23 +51,31 @@ public class film_details_maintain extends AppCompatActivity {
         btndelete = findViewById(R.id.idbtnDelete);
 
         txtfilmname = (EditText) findViewById(R.id.idtxtFilmName);
-        viewFilmName = (TextView) findViewById(R.id.idtxtFilmName);
-
 
         txtrole1 = (EditText) findViewById(R.id.idtxtRole1);
-        viewRole1 = (TextView) findViewById(R.id.idtxtRole1);
 
         txtrole2 = (EditText) findViewById(R.id.idtxtRole2);
-        viewRole2 = (TextView) findViewById(R.id.idtxtRole2);
 
-        txtrole3 = (EditText) findViewById(R.id.idtxtRole3);
-        viewRole3 = (TextView) findViewById(R.id.idtxtRole3);
+       txtrole3 = (EditText) findViewById(R.id.idtxtRole3);
 
         txtrole4 = (EditText) findViewById(R.id.idtxtRole4);
-        viewRole4 = (TextView) findViewById(R.id.idtxtRole4);
 
         txtdirector = (EditText) findViewById(R.id.idtxtDirector);
-        viewDirector = (TextView) findViewById(R.id.idtxtDirector);
+
+        image1 = (TextView) findViewById(R.id.idtxtphoto1);
+        viewimage1 = (ImageView) findViewById(R.id.idimage1);
+
+        image2 = (TextView) findViewById(R.id.idtxtphoto2);
+        viewimage2 = (ImageView) findViewById(R.id.idimage2);
+
+        image3 = (TextView) findViewById(R.id.idtxtphoto3);
+        viewimage3 = (ImageView) findViewById(R.id.idimage3);
+
+        image4 = (TextView) findViewById(R.id.idtxtphoto4);
+        viewimage4 = (ImageView) findViewById(R.id.idimage4);
+
+
+
 
 
     }
@@ -85,5 +98,8 @@ public class film_details_maintain extends AppCompatActivity {
     public void ShowToastDelete(View view) {
         Toast toastAdd = Toast.makeText(getApplicationContext(),"Delete sucessfully",Toast.LENGTH_LONG);
         toastAdd.show();
+    }
+
+    public void btn_click(View view) {
     }
 }
