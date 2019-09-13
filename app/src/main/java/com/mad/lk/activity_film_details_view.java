b01a6c1340e6c9ffdbbe672cce7e192d4b5fc145
SBA <buddikasamesh1@gmail.com>
2019-09-13 09:06:34 +0530

    Search for films function was implemented

--- app/src/main/java/com/mad/lk/activity_film_details_view.java
+++ app/src/main/java/com/mad/lk/activity_film_details_view.java
@@ -8,11 +8,13 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.jgabrielfreitas.core.BlurImageView;
 import com.mad.lk.Film_Details_Maintain.Utils.Utils;
 
 
@@ -21,12 +23,13 @@ public class activity_film_details_view extends AppCompatActivity {
 
 
     TextView filmName,Ratings,Date,Time,Seats,Description;
-    ConstraintLayout layout;
+    ImageView poster;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_film_details_view);
+
 
         filmName = (TextView) findViewById(R.id.filmName);
         Ratings = (TextView) findViewById(R.id.Ratings);
@@ -34,24 +37,28 @@ public class activity_film_details_view extends AppCompatActivity {
         Time = (TextView) findViewById(R.id.Time);
         Seats = (TextView) findViewById(R.id.Seats);
         Description = (TextView) findViewById(R.id.Description);
+        poster = (ImageView) findViewById(R.id.poster);
 
-        layout = (ConstraintLayout) findViewById(R.id.layout);
+        poster.setImageBitmap(SearchForTickets.background);
 
         Intent intent = getIntent();
         filmName.setText(intent.getStringExtra("NAME"));
-        Ratings.setText(intent.getStringExtra("RANKINGS"));
-        Date.setText(intent.getStringExtra("DATE"));
-        Time.setText(intent.getStringExtra("TIME"));
-        Seats.setText(intent.getStringExtra("SEATS"));
+        Ratings.setText("Rankings : "+intent.getStringExtra("RANKINGS"));
+        Date.setText("Date : " + intent.getStringExtra("DATE"));
+        Time.setText("Time : "+intent.getStringExtra("TIME"));
+        Seats.setText("Seats : " + intent.getIntExtra("SEATS",0));
         Description.setText(intent.getStringExtra("DESCRIPTION"));
 
-        Drawable d = new BitmapDrawable(getResources(), SearchForTickets.background);
 
-        layout.setBackground(d);
-        layout.setAlpha(.5f);
 
 
 
     }
 
+
+    public void BookTicket(View view)
+    {
+        Intent intent = new Intent(getApplicationContext(),bookTicket.class);
+        startActivity(intent);
+    }
 }
