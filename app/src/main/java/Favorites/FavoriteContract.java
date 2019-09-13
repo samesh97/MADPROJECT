020a0e6538ca57253cd1283df44d87a4bcc30957
TharashaMiyu <tharasha.miyu@gmail.com>
2019-09-13 17:44:17 +0530

    sep13-2

---
+++ app/src/main/java/Favorites/FavoriteContract.java
@@ -0,0 +1,4 @@
+package Favorites;
+
+public class FavoriteContract {
+}
---
+++ app/src/main/java/Favorites/FavoriteEntry.java
@@ -0,0 +1,17 @@
+package Favorites;
+
+public class FavoriteEntry {
+
+
+    public static final String DATABASE_NAME = "Favorite.db";
+
+    public static final String TABLE_NAME = "Favorite_Details";
+
+    public static final String FILM_COL_1 = "NAME";
+    public static final String FILM_COL_2 = "DESCRIPTION";
+    public static final String FILM_COL_3 = "RANKINGS";
+    public static final String FILM_COL_4 = "DATE";
+    public static final String FILM_COL_5 = "TIME";
+    public static final String FILM_COL_6 = "SEATS";
+    public static final String FILM_COL_7 = "NOTES";
+}
